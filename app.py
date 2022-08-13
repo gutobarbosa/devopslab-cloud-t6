7ee4c0371f9c390ddffa201fb698eccfa93dd81a
Augusto Barbosa Andrade dos Santos <46411054+gutobarbosa@users.noreply.github.com>
2022-08-13 11:23:45 -0300

    Desafio: Custom Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Subindo uma nova mensagem para producao!"
+    return "Subindo uma nova mensagem para producao! Alou mundao..."
 
 if __name__ == '__main__':
     app.run()
